ae655dca12f44ea506092448fa9674b2799cbc91
Taras <tarikkataryna1999@gmail.com>
2018-09-16 20:50:21 +0300

    Added CurrencyConverter and made some changes in Read() method (string path changed to StreamReader streamReader in method's parametrs)

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task1
 {
@@ -10,7 +11,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            
+           
         }
     }
 }
