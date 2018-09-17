6cc4f57d42dbe18359289537ef0741c86fee5827
Eduard <edikzaxarin@gmail.com>
2018-09-17 14:32:08 +0300

    Modified class CurrencyConverter(Made it static and added new method);Added static class Tasks;Completed working on class Tasks);

--- Task1/CurrencyConverter.cs
+++ Task1/CurrencyConverter.cs
@@ -6,32 +6,91 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    class CurrencyConverter
+    static class CurrencyConverter
     {
-        public Currency ConvertTo(string curToConvert,Currency currency)
+        public static Currency ConvertTo2(CurrencyName currName,List<Currency> list)
+        {
+            
+            double sumOfAll = default(double);
+            list.Sort((Currency a1, Currency a2) =>
+            {
+               return a1.CurrName.CompareTo(a2.CurrName);
+            });
+            if(currName == CurrencyName.Grivna)
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i].CurrName == currName)
+                            sumOfAll += list[i].Amount;
+                        else if(list[i].CurrName==CurrencyName.Dollar)
+                        {
+                            sumOfAll += list[i].Amount * 27;
+                        }
+                        else
+                        {
+                            sumOfAll += list[i].Amount * 30;
+                        }
+                    }
+                }
+            else if(currName == CurrencyName.Dollar)
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i].CurrName == currName)
+                            sumOfAll += list[i].Amount;
+                        else if (list[i].CurrName == CurrencyName.Grivna)
+                        {
+                            sumOfAll += list[i].Amount / 27;
+                        }
+                        else
+                        {
+                            sumOfAll += list[i].Amount * 1.2;
+                        }
+                    }
+                }
+            else
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i].CurrName == currName)
+                            sumOfAll += list[i].Amount;
+                        else if (list[i].CurrName == CurrencyName.Dollar)
+                        {
+                            sumOfAll += list[i].Amount * 0.8;
+                        }
+                        else
+                        {
+                            sumOfAll += list[i].Amount / 30;
+                        }
+                    }
+                }
+            return new Currency(sumOfAll, currName);                      
+        }
+        public static Currency ConvertTo(string curToConvert,Currency currency)
         {
             double amount = 0;
+            amount = currency.Amount;
             string cur = string.Empty;
-            if (curToConvert == "$")
+            if (curToConvert == "$"||curToConvert == "dol")
             {
-                if (currency.CurrencyName == "E")
-                    return new Currency(amount * 0.8, "E");
+                if (currency.CurrName == CurrencyName.Euro)
+                    return new Currency(amount * 0.8, CurrencyName.Euro);
                 else
-                    return new Currency(amount * 27, "grn");
+                    return new Currency(amount * 27, CurrencyName.Dollar);
             }
-            else if (curToConvert == "E")
+            else if (curToConvert == "E"||curToConvert == "eur")
             {
-                if (currency.CurrencyName == "$")
-                    return new Currency(amount * 1.2, "$");
+                if (currency.CurrName == CurrencyName.Dollar)
+                    return new Currency(amount * 1.2,CurrencyName.Dollar);
                 else
-                    return new Currency(amount * 30, "grn");
+                    return new Currency(amount * 30, CurrencyName.Grivna);
             }
             else
             {
-                if (currency.CurrencyName == "E")
-                    return new Currency(amount / 30, "E");
+                if (currency.CurrName == CurrencyName.Euro)
+                    return new Currency(amount / 30, CurrencyName.Euro);
                 else
-                    return new Currency(amount / 27, "$");
+                    return new Currency(amount / 27, CurrencyName.Dollar);
             }
         }
     }
