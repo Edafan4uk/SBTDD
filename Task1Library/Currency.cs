6957dbad3cb8b9a35101285f158c3cbe869657dd
ihorHuralskyy <uniqueihor@gmail.com>
2018-09-18 20:08:10 +0300

    Added class library and unit tests.Fixed some bugs

---
+++ Task1Library/Currency.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    public enum CurrencyName
+    {
+        Grivna,
+        Dollar,
+        Euro
+    }
+    public class Currency : IReadable
+    {
+        public bool HasMissingField { get; private set; }
+        public CurrencyName CurrName { get; private set; }
+        public double Amount { get; private set; }
+        public Currency()
+        {
+            HasMissingField = false;
+            CurrName = default(Task1.CurrencyName);
+            Amount = 0;
+        }
+        public Currency(double am, CurrencyName cur)
+        {
+            HasMissingField = false;
+            Amount = am;
+            CurrName = cur;
+        }
+        public void Read(StreamReader streamReader)
+        {
+            string temp = streamReader.ReadLine();
+            string[] splitString = temp.Split(',', ' ', ';');
+            double amount = 0;
+            try
+            {
+                if (splitString[0] == string.Empty || splitString[1] == string.Empty)
+                    throw new ArgumentNullException();
+                else if ((!double.TryParse(splitString[0], out amount)))
+                    throw new FormatException();
+                else
+                {
+                    Amount = amount;
+                    string currName = splitString[1];
+                    switch (currName)
+                    {
+                        case "grn":
+                            CurrName = Task1.CurrencyName.Grivna;
+                            break;
+                        case "eur":
+                            CurrName = Task1.CurrencyName.Euro;
+                            break;
+                        case "dol":
+                            CurrName = Task1.CurrencyName.Dollar;
+                            break;
+                        default:
+                            HasMissingField = true;
+                            throw new Exception($"The is no such currency as { currName}");
+                    }
+                }
+            }
+            catch (ArgumentNullException argNullEx)
+            {
+                Console.WriteLine(argNullEx.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format($"Currency: {CurrName.ToString()};Amount: {Amount}");
+        }
+    }
+}
---
+++ Task1Tests/CurrencyTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+using System.IO;
+
+
+namespace Task1Tests
+{
+    [TestClass]
+    public class CurrencyTest
+    {
+        [TestMethod]
+        public void CurrencyConstructorTest()
+        {
+            Currency cur = new Currency(3.5, CurrencyName.Dollar);
+
+            Assert.IsTrue(cur.Amount == 3.5 && cur.CurrName == CurrencyName.Dollar);
+        }
+        [TestMethod]
+        public void CurrencyReadMethodTest()
+        {
+            StreamReader sr = File.OpenText(@"C:\GitHub\SBTDD\TestFile.txt");
+            Currency cur = new Currency();
+
+            cur.Read(sr);
+
+            Assert.IsTrue(cur.Amount == 212 && cur.CurrName == CurrencyName.Grivna);
+        }
+        [TestMethod]
+        public void CurrencyToStringTest()
+        {
+            Currency cur = new Currency(10, CurrencyName.Euro);
+
+            Assert.AreEqual(cur.ToString(), "Currency: Euro;Amount: 10");
+        }
+    }
+}
